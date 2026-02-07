08c124e1f562b9b6a78226d230652fd98d3f1f89
Alexandre Braz Franca <barazalexandre@gmail.com>
2026-02-07 14:50:10 -0300

    feito exercicios irados de vetore

--- assets/js/vetor-for.js
+++ assets/js/vetor-for.js
@@ -137,15 +137,15 @@ function jogoPalavras() {
     let palavras = ["Roblox", "Free Fire", "GTA"];
     let nomeJogador = prompt("Digite seu nome de jogador");
     let quantidadeTentativas = 10;
-    
+
     if (nomeJogador.toLowerCase().includes("Lorenzo")) {
         quantidadeTentativas = 3;
     }
 
-    while(quantidadeTentativas >= 0) {
-        let nomeJogo  = prompt("Tente adivinhar o nome dos 3 jogos");
+    while (quantidadeTentativas >= 0) {
+        let nomeJogo = prompt("Tente adivinhar o nome dos 3 jogos");
         //Verificando se o nome que o usuário digitou está dentro da lista
-        if(palavras.includes(nomeJogo)) {
+        if (palavras.includes(nomeJogo)) {
             //Dar mais 3 tentativas a cada acerto
             quantidadeTentativas += 3;
             //Como descobrir a posição
@@ -154,22 +154,229 @@ function jogoPalavras() {
             palavras.splice(indiceJogo, 1);
             console.log(palavras);
             alert(`Acertou ${nomeJogo}`);
-            if (palavras.length === 0){
+            if (palavras.length === 0) {
                 alert("Ganhou o jogo");
                 break;
             }
-        }else {
+        } else {
             //Decrementar em 1
             quantidadeTentativas -= 1;
             alert(`Errou\nVocê tem ${quantidadeTentativas} tentativas restantes`);
         }
         console.log("Quantidade de tentativas: " + quantidadeTentativas + "\npalavras: " + palavras);
-        if(quantidadeTentativas <= 0){
+        if (quantidadeTentativas <= 0) {
             alert("Perdeu o jogo");
             break;
         }
     }
 }
+
+function exemploCorTinta() {
+    let tintas = []
+
+    //Solicitando o nome de 5 tintas
+    for (let i = 0; i < 5; i = i + 1) {
+        //Solicitando o nome da tinta
+        let nomeTinta = prompt("Digite o nome da cor de tinta");
+        //Adicionando (salvando) o nome da tinta no vetor
+        tintas.push(nomeTinta);
+    }
+
+    let texto = "";
+    for (let i = 0; i < 5; i = i + 1) {
+        // texto = texto + tintas [1];
+        texto += tintas[i] + "\n";
+    }
+
+    alert("Tintas:\n" + texto);
+}
+
+function alunos() {
+    let alunos = []
+
+    for (let i = 0; i < 13; i = i + 1) {
+        let nomeAluno = prompt("Digite o nome do aluno");
+        // if (nomeAluno === Lorenzo){
+        // continue;
+        // }
+
+        alunos.push(nomeAluno);
+    }
+
+    alert("Qtd de alunos: " + alunos.length);
+
+    let resultado = "";
+    //percorrer a lista
+    for (let i = 0; i < 13; i = i + 1) {
+        resultado += alunos[i] + "\n";
+
+        alert(resultado);
+        //Pegar o nome do aluno
+        // let menorNomeAluno = alunos[0];
+        let menorNomeAluno = "sdakndksnkdnksasdaskdjskajkdjsakjkdaskljdklsjakldjsajdka";
+        let nomeAlunoPercorrido = alunos[i];
+        //Verificar se o tamanho do nome do aluno é menor que o tamanho do menor de aluno
+        if (nomeAlunoPercorrido.length < menorNomeAluno.length) {
+            // Caso sim: atualizar o menorNomeAluno com o nome do aluno
+            menorNomeAluno = nomeAlunoPercorrido;
+        }
+    }
+
+    alert("Aluno com menor nome de aluno: " + menorNomeAluno);
+}
+
+/*
+Ex 1: Criar uma função exercicio01, solicitando o nome dos smartphones
+    - Criar uma lista com o nome dos smartphones (vazia)
+    - Solicitar o nome de 5 smartphones (utilizar for) e armazenar em uma lista
+    - Apresentar o nome dos 5 smartphones
+Ex 2: Conti. Ex 1: Adicionar uma lista de quantidadeEstoque (vazia)
+    - Modificar o for de solicitação do nome dos 5 smartphones para solicitar 
+    também a quantidade de estoque 
+    - Modificar a apresentação para apresentar a quantidade de estoque daquele produto
+Ex 3: Conti. Ex 1: Deve apresentar um status de como está o estoque em si
+    - Status: 
+        Elevado     10
+        Estável     7
+        Preocupante 4
+        Urgente     1
+    De acordo com a quantidade estoque de cada smartphone deve apresentar seu status
+*/
+
+function ex01() {
+    let smartphone = [];//indice
+    let estoque = [];
+    let status = [];
+    let statusFinal = "";
+    let menorQuantidade = 99999;
+    let maiorQuantidade = 0;
+    let valor = [];
+
+
+    for (let i = 0; i < 5; i = i + 1) {
+        //solicitar nome smartphone
+        let nomeSmartphone = prompt("Digite o nome do smartphone");
+        // pede quantidade em estoque
+        let quantidadeEstoque = parseInt(prompt("Digite a quantidade do item " + nomeSmartphone + " em estoque"));
+        let preco = parseFloat(prompt("Digite o valor do " + nomeSmartphone));
+        //salva dados
+        smartphone.push(nomeSmartphone);
+        estoque.push(quantidadeEstoque);
+        valor.push(preco)
+
+        if (estoque[i] > 10) {
+            statusFinal = "Elevado"
+        } else if (estoque[i] >= 7) {
+            statusFinal = "Estável"
+        } else if (estoque[i] >= 4) {
+            statusFinal = "Preocupante"
+        } else if (estoque[i] >= 1) {
+            statusFinal = "Urgente"
+        }
+        status.push(statusFinal);
+    };
+
+    for (let i = 0; i < 5; i = i + 1) {
+        if (estoque[i] < menorQuantidade) {
+            menorQuantidade = estoque[i];
+        }
+    }
+
+    for (let i = 0; i < 5; i = i + 1) {
+        if (estoque[i] > maiorQuantidade) {
+            maiorQuantidade = estoque[i];
+        }
+    }
+
+
+
+    let smartResulta = "";
+    for (let i = 0; i < 5; i = i + 1) {
+        smartResulta += smartphone[i] + "\n";
+        smartResulta += estoque[i] + "\n";
+        smartResulta += valor[i] + "\n";
+        smartResulta += status[i] + "\n";
+
+    }
+    alert(smartResulta);
+    alert("Menor valor do estoque " + menorQuantidade);
+    alert("Maior valor do estoque " + maiorQuantidade);
+
+}
+function ex02() {
+    let produto = [];
+    let categorias = [];
+    let valorProduto = [];
+    let classificacao = [];
+    let classificacaoFinal = "";
+    let menorPreco = 99999;
+    let produtoMenorPreco = "";
+    let maiorPreco = 0;
+    let descontos = [];
+
+    for (let i = 0; i < 5; i = i + 1){
+        let nomeproduto = prompt("Digite o nome do produto");
+        let categoriaproduto = prompt("O produto faz parte de qual categoria");
+        let preco = parseFloat(prompt("Digite o valor"));
+        let desconto = parseInt(prompt("Digite quanto por cento de desconto que tem no produto (%)"));
+        produto.push(nomeproduto);
+        categorias.push(categoriaproduto);
+        valorProduto.push(preco);
+
+        if (valorProduto[i] > 100) {
+            classificacaoFinal = "Caro"
+        } else if (valorProduto[i] >= 50 < 100) {
+            classificacaoFinal = "Médio"
+        } else if (valorProduto[i] >= 20 < 50) {
+            classificacaoFinal = "Barato"
+        } else if (valorProduto[i] < 20) {
+            classificacaoFinal = "Muito Barato"
+        }
+        classificacao.push(classificacaoFinal);
+        
+        for (let i = 0; i < 5; i = i + 1) {
+            if (valorProduto[i] > maiorPreco) {
+                maiorPreco = valorProduto[i];
+            }
+        }
+
+        for (let i = 0; i < 5; i = i + 1) {
+            if (valorProduto[i] < menorPreco) {
+                menorPreco = valorProduto[i];
+
+                produtoMenorPreco = valorProduto;
+
+            }
+        }
+
+
+    }
+    let produtoResultado = "";
+    for (let i = 0; i < 5; i = i + 1){
+        produtoResultado += "Produto: " + produto[i] + "\n"
+        produtoResultado += "Categoria: " + categorias[i] + "\n"
+        produtoResultado += "Preço: R$ " + valorProduto[i] + "\n"
+        produtoResultado += "Classificação: " + classificacao[i] + "\n"
+        
+
+    }alert(produtoResultado);
+    alert("Maior valor da compra " + maiorPreco);
+    alert("Menor valor da compra " + menorPreco);
+    alert("O produto de menor valor é " + produtoMenorPreco + valorProduto);
+}
+
+
+
+
+
+
+// Calcular e apresentar o preço final:
+
+// precoFinal = preco - (preco * desconto/100)
+
+// Apresentar:
+// Produto: <nome> | Preço: R$ <preco> | Desc: <desconto>% | Final: R$ <precoFinal></precoFinal>
+
 
 //        [0, 1, 2, 3]
 // numeros = [1, 3, 4, 9]
